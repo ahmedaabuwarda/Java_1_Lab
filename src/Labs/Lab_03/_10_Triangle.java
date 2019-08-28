6a234496039614eb547a7a2f0c9a8f6dd82d12e5
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2019-08-29 01:36:49 +0300

    Update _10_Triangle.java file.

--- src/Labs/Lab_03/_10_Triangle.java
+++ src/Labs/Lab_03/_10_Triangle.java
@@ -7,21 +7,38 @@ package Labs.Lab_03;
 
 import java.util.Scanner;
 
+/**
+ * @author Ahmed Abuwarda, Date: Aug 29,2019.
+ */
 public class _10_Triangle {
+
+    /**
+     * @param args the command line here.
+     */
     public static void main(String[] args) {
+        // TODO code application logic here.
 
         Scanner input = new Scanner(System.in);
+
         System.out.print("\n  >> Enter Edge1: ");
         double edge1 = input.nextDouble();
+
         System.out.print("  >> Enter Egde2: ");
         double edge2 = input.nextDouble();
+
         System.out.print("  >> Enter Edge3: ");
         double edge3 = input.nextDouble();
+
         if (edge1 + edge2 > edge3 && edge1 + edge3 > edge2 && edge3 + edge2 > edge1) {
+
             System.out.println("  >> The Perimeter is: " + (edge1 + edge2 + edge3));
+
         } else {
+
             System.out.println("  >> The input is invalid !");
+
         }
 
     }
+
 }
