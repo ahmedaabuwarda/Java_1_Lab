39a32ee24ee7edf124f5fac43c21d073442653e4
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2019-08-29 01:34:48 +0300

    Update _03_if_Statements.java file.

--- src/Labs/Lab_03/_03_if_Statements.java
+++ src/Labs/Lab_03/_03_if_Statements.java
@@ -7,17 +7,33 @@ package Labs.Lab_03;
 
 import java.util.Scanner;
 
+/**
+ * @author Ahmed Abuwarda, Date: Aug 29,2019.
+ */
 public class _03_if_Statements {
+
+    /**
+     * @param args the command line here.
+     */
     public static void main(String[] args) {
+        // TODO code application logic here.
 
         Scanner input = new Scanner(System.in);
+
         System.out.print("\n  >> Enter radius: ");
         double radius = input.nextDouble();
+
         if (radius >= 0) {
+
             double area = 2 * radius * Math.PI;
             System.out.println("  >> The area is: " + area);
+
         } else {
+
             System.out.println("  >> radius cannot be negative !!");
+
         }
+
     }
+
 }
