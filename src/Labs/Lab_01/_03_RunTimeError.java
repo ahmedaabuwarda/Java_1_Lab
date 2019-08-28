7f09e0d0470cfbdaddf1d813758e7c1d5cbea065
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2019-08-29 01:15:10 +0300

    Update _03_RunTimeError.java file.

--- src/Labs/Lab_01/_03_RunTimeError.java
+++ src/Labs/Lab_01/_03_RunTimeError.java
@@ -1,7 +1,20 @@
+/**
+ * To change this template file, choose Settings | Editor | File and Code Templates
+ * and change the template in the editor.
+ */
+
 package Labs.Lab_01;
 
+/**
+ * @author Ahmed Abuwarda, Date: Aug 29,2019.
+ */
 public class _03_RunTimeError {
+
+    /**
+     * @param args the command line here.
+     */
     public static void main(String[] args) {
+        // TODO code application logic here.
 
         // System.out.println(1 / 0);
 
@@ -9,4 +22,5 @@ public class _03_RunTimeError {
                 " >>> Divide by Zero is Infinity");
 
     }
+
 }
