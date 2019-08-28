1788db4e7337b2e87021243a89915016535163c0
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2019-08-29 01:21:49 +0300

    Update _07_IncAndDecOperators.java file.

--- src/Labs/Lab_02/_07_IncAndDecOperators.java
+++ src/Labs/Lab_02/_07_IncAndDecOperators.java
@@ -18,24 +18,19 @@ public class _07_IncAndDecOperators {
 
         int i = 1;
         int j = 0;
-        System.out.println("\n i = 1 \n" +
-                " j = 0 \n" + "\n j = ++i  >>  " + (j = ++i) +
-                "\n i = " + i);
+        System.out.println("\n i = 1 \n" + " j = 0 \n" + "\n j = ++i  >>  " + (j = ++i) + "\n i = " + i);
 
         i = 1;
         j = 0;
-        System.out.println(" j = i++  >>  " + (j = i++) +
-                "\n i = " + i);
+        System.out.println(" j = i++  >>  " + (j = i++) + "\n i = " + i);
 
         i = 1;
         j = 0;
-        System.out.println(" j = i--  >>  " + (j = i--) +
-                "\n i = " + i);
+        System.out.println(" j = i--  >>  " + (j = i--) + "\n i = " + i);
 
         i = 1;
         j = 0;
-        System.out.println(" j = --i  >>  " + (j = --i) +
-                "\n i = " + i);
+        System.out.println(" j = --i  >>  " + (j = --i) + "\n i = " + i);
 
     }
 
