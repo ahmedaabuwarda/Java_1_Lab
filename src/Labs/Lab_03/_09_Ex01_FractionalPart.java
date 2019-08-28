a054d4d2db6bb3bcd6581efcd1c4cbf8b3f87d5b
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2019-08-29 01:36:32 +0300

    Update _09_Ex01_FractionalPart.java file.

--- src/Labs/Lab_03/_09_Ex01_FractionalPart.java
+++ src/Labs/Lab_03/_09_Ex01_FractionalPart.java
@@ -7,17 +7,32 @@ package Labs.Lab_03;
 
 import java.util.Scanner;
 
+/**
+ * @author Ahmed Abuwarda, Date: Aug 29,2019.
+ */
 public class _09_Ex01_FractionalPart {
+
+    /**
+     * @param args the command line here.
+     */
     public static void main(String[] args) {
+        // TODO code application logic here.
 
         Scanner input = new Scanner(System.in);
+
         System.out.print("\n  >> Enter a number: ");
         double number = input.nextInt();
+
         if (number % 1 == 0) {
+
             System.out.println("  >> fractional part is zero");
+
         } else {
+
             System.out.println("  >> fractional part is not zero");
+
         }
 
     }
+
 }
