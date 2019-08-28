2e9659db5fa0bac9f9c16a71a322052afd58fffa
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2019-08-29 01:35:41 +0300

    Update _06_LogicalOperators.java file.

--- src/Labs/Lab_03/_06_LogicalOperators.java
+++ src/Labs/Lab_03/_06_LogicalOperators.java
@@ -5,14 +5,21 @@
 
 package Labs.Lab_03;
 
+/**
+ * @author Ahmed Abuwarda, Date: Aug 29,2019.
+ */
 public class _06_LogicalOperators {
-    public static void main(String[] args) {
 
-        System.out.println("\n Operator \t Name \t\t\t Description" +
-                "\n ! \t\t\t not \t\t\t logical negation" +
-                "\n && \t\t and \t\t\t logical conjunction" +
-                "\n || \t\t or \t\t\t logical disjunction" +
+    /**
+     * @param args the command line here.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here.
+
+        System.out.println("\n Operator \t Name \t\t\t Description" + "\n ! \t\t\t not \t\t\t logical negation" +
+                "\n && \t\t and \t\t\t logical conjunction" + "\n || \t\t or \t\t\t logical disjunction" +
                 "\n ^ \t\t\t exclusive or \t logical exclusion");
 
     }
+
 }
