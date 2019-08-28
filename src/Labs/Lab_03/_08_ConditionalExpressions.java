13237334941c8a2da50f2e6fe9b12c6ff03ef92c
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2019-08-29 01:36:15 +0300

    Update _08_ConditionalExpressions.java file.

--- src/Labs/Lab_03/_08_ConditionalExpressions.java
+++ src/Labs/Lab_03/_08_ConditionalExpressions.java
@@ -7,16 +7,27 @@ package Labs.Lab_03;
 
 import java.util.Scanner;
 
+/**
+ * @author Ahmed Abuwarda, Date: Aug 29,2019.
+ */
 public class _08_ConditionalExpressions {
+
+    /**
+     * @param args the command line here.
+     */
     public static void main(String[] args) {
+        // TODO code application logic here.
 
         // Conditional Expressions...
         System.out.println("\n  >> The syntax is: " +
                 "boolean-expression ? true-expression : false-expression;");
+
         Scanner input = new Scanner(System.in);
         System.out.print("\n  >> Enter a number: ");
         int number = input.nextInt();
+
         System.out.println(number % 2 == 0 ? "  >> The Number is: Even" : "  >> The Number is: Odd");
 
     }
+
 }
