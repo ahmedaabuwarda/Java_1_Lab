75dcd00d93c66d574247c5939cadc1f76911bf55
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2019-08-29 01:21:03 +0300

    Update _06_AugmentedAssignmentOperators.java file.

--- src/Labs/Lab_02/_06_AugmentedAssignmentOperators.java
+++ src/Labs/Lab_02/_06_AugmentedAssignmentOperators.java
@@ -28,8 +28,8 @@ public class _06_AugmentedAssignmentOperators {
         a += a + 1;     // becomes >> a = a + a + 1.
         System.out.println("\n " + a);      // prints 14.0.
 
-        a = 6;      // Assign a new value to a.
-        a /= 2;     // becomes >> a = a / 2.
+        a = 6;       // Assign a new value to a.
+        a /= 2;      // becomes >> a = a / 2.
         System.out.println("\n " + a);      // prints 3.0.
 
     }
