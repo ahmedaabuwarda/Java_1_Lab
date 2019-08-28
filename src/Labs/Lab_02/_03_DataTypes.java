a17efb0803860ae47109b1bd9f693d9d85404156
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2019-08-29 01:18:11 +0300

    Update _03_DataTypes.java file.

--- src/Labs/Lab_02/_03_DataTypes.java
+++ src/Labs/Lab_02/_03_DataTypes.java
@@ -16,14 +16,13 @@ public class _03_DataTypes {
     public static void main(String[] args) {
         // TODO code application logic here.
 
-        System.out.println("\n" +
-            " Name     Storage Size \n" +
-            " byte     8 \n" +
-            " short    16 \n" +
-            " int      32 \n" +
-            " long     64 \n" +
-            " float    32 \n" +
-            " double   64");
+        System.out.println("\n" + " Name     Storage Size \n" +
+                " byte     8 \n" +
+                " short    16 \n" +
+                " int      32 \n" +
+                " long     64 \n" +
+                " float    32 \n" +
+                " double   64");
 
     }
 
