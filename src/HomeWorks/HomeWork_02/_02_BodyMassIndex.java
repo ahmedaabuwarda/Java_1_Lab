437e4c181ab9501f69e38000535521831494af8c
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2019-08-29 00:47:39 +0300

    Update _02_BodyMassIndex.java file.

--- src/HomeWorks/HomeWork_02/_02_BodyMassIndex.java
+++ src/HomeWorks/HomeWork_02/_02_BodyMassIndex.java
@@ -21,8 +21,10 @@ public class _02_BodyMassIndex {
         Scanner input = new Scanner(System.in);
         System.out.print("\n  >> Enter weight in pounds: ");
         double weight = input.nextDouble();
+
         System.out.print("  >> Enter height in inches: ");
         double height = input.nextDouble();
+
         double kilo = weight * 0.45359237;
         double meter = height * 0.0254;
         double BMI = kilo / (meter * meter);
