c0b00f51742e4040f114cf61cf26a98607618984
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2019-08-29 01:14:44 +0300

    Update _02_SyntaxError.java file.

--- src/Labs/Lab_01/_02_SyntaxError.java
+++ src/Labs/Lab_01/_02_SyntaxError.java
@@ -1,12 +1,26 @@
+/**
+ * To change this template file, choose Settings | Editor | File and Code Templates
+ * and change the template in the editor.
+ */
+
 package Labs.Lab_01;
 
+/**
+ * @author Ahmed Abuwarda, Date: Aug 29,2019.
+ */
 public class _02_SyntaxError {
-    public static void main(String[] args) {
 
-        // System.out.println("\n Hello World!";  <<  Error!
+    /**
+     * @param args the command line here.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here.
+
+        // System.out.println("\n Hello World!";  <<  Error!.
 
         System.out.print("\n System.out.println(\"Hello World!\";");
         System.out.print(" >>> Syntax Error >>> Missing \")\" \n");
 
     }
+
 }
