a26eb2d9792643891b0a68fe6b7cba1508777999
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2019-08-29 01:14:09 +0300

    Update _01_Main.java file.

--- src/Labs/Lab_01/_01_Main.java
+++ src/Labs/Lab_01/_01_Main.java
@@ -1,7 +1,20 @@
+/**
+ * To change this template file, choose Settings | Editor | File and Code Templates
+ * and change the template in the editor.
+ */
+
 package Labs.Lab_01;
 
+/**
+ * @author Ahmed Abuwarda, Date: Aug 29,2019.
+ */
 public class _01_Main {
+
+    /**
+     * @param args the command line here.
+     */
     public static void main(String[] args) {
+        // TODO code application logic here.
 
         System.out.println("\n Hello World! \n");
 
@@ -10,4 +23,5 @@ public class _01_Main {
                 "\n (17 / (3.2 - 5))");
 
     }
+
 }
