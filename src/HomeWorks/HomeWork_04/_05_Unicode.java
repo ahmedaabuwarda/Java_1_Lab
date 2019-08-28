28c5e37c0cdb84fc34feff860597f0316c10952d
Ahmed Abuwarda <zzz.sleep.com@gmail.com>
2019-08-29 00:55:51 +0300

    Update _05_Unicode.java file.

--- src/HomeWorks/HomeWork_04/_05_Unicode.java
+++ src/HomeWorks/HomeWork_04/_05_Unicode.java
@@ -8,7 +8,7 @@ package HomeWorks.HomeWork_04;
 import java.util.Scanner;
 
 /**
- * @author Ahmed Abuwarda on: Aug 29,2019.
+ * @author Ahmed Abuwarda, Date: Aug 29,2019.
  */
 public class _05_Unicode {
 
@@ -19,6 +19,7 @@ public class _05_Unicode {
         // TODO code application logic here.
 
         Scanner input = new Scanner(System.in);
+
         System.out.print("\n  >> Enter your Character: ");
         String cha = input.nextLine();
 
